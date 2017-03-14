d0a6296189147b32084ee35f66af805d4da748aa
Felix E. C. Klemke <Fklemke@Felixs-MacBook-Pro.local>
2017-03-14 10:45:16 +0100

    working example

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,4 +1,6 @@
 import Vapor
+//import VaporPostgreSQL
+
 
 let drop = Droplet()
 
@@ -7,6 +9,32 @@ drop.get { req in
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
 }
+//
+//drop.get("friends") { req in
+//    return try JSON(node: ["friends": [
+//        ["name":"Sven", "nationality by heart": "where the kivis grow"],
+//        ["name":"gregor", "nationality by heart": "shakes & fitches"],
+//        ["name":"dustin", "nationality by heart": "RUST"],
+//        ["name":"jakob", "nationality by heart": "RICH JEW"],
+//        ["name":"max", "nationality by heart": "Belgian Banana Republic"],
+//        ["name":"dominik", "nationality by heart": "Zigeuner"],
+//        ]
+//        ])
+//}
+
+
+drop.get("friends") { req in
+    let friends = [Friend(name: "Sven", nationalityByHeart: "Where the Kivis grow", email: "6kiviaday@kiviislife.com"),
+        Friend(name: "Dustin", nationalityByHeart: "RUST", email: "rust@rustmyrust.com"),
+        Friend(name: "Jakob", nationalityByHeart: "Rich Jew", email: "momoney@moproblems.com"),
+        Friend(name: "Gregor", nationalityByHeart: "H-Town", email: "bossplayer@1305plattenbauHunter.com")
+    ]
+
+    let friendsNode = try friends.makeNode()
+    let nodeDictionary = ["friends": friendsNode]
+    return try JSON(node: nodeDictionary)
+}
+
 
 drop.resource("posts", PostController())
 
